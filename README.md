879e39ce69dc79b14ac1dd8069b4e528eff7afcf
Dmitry Erohin <nefariusmag@gmail.com>
2018-12-15 10:39:14 +0000

    Создать БД #1

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # virtual-wardrobe
 
+Create DataBase on Postgres (ver.9.6)
+
+Add ENV in database.py
+
+pip install -r requirements.txt
+
---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+db_host = os.getenv('DB_HOST', 'localhost')
+db_name = os.getenv('DB_NAME', 'postgres')
+db_login = os.getenv('DB_LOGIN', 'postgres')
+db_password = os.getenv('DB_PASSWORD', 'postgres')
