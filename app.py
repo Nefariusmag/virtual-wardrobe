5fbcd66302cf697041687d79cd20b1e2b7770d7e
DEShmelev <arknitos@gmail.com>
2018-12-15 12:58:18 +0300

    Ref #6 Make Weather as a single class

--- app.py
+++ app.py
@@ -25,53 +25,54 @@ class User(object):
 
         return user_geo
 
-    class Weather(object):
-            def __init__(self, city='Moscow,ru', lang='en'):
-                self._city = city
-                self._lang = lang
-                self.weather = self.get_weather()
 
-            def get_weather(self):
-                from config import WEATHER_APIKEY, WEATHER_URL
-                params = {
-                    'q': self._city,
-                    'APPID': WEATHER_APIKEY,
-                    'units': 'metric',
-                    'lang': self._lang
-                }
-                _weather = requests.get(WEATHER_URL, params)
+class Weather(object):
+        def __init__(self, city='Moscow,ru', lang='en'):
+            self._city = city
+            self._lang = lang
+            self.weather = self.get_weather()
 
-                if _weather.status_code == 200:
-                    _weather = _weather.json()
+        def get_weather(self):
+            from config import WEATHER_APIKEY, WEATHER_URL
+            params = {
+                'q': self._city,
+                'APPID': WEATHER_APIKEY,
+                'units': 'metric',
+                'lang': self._lang
+            }
+            _weather = requests.get(WEATHER_URL, params)
 
-                conditions, temp, humidity = '', '', ''
+            if _weather.status_code == 200:
+                _weather = _weather.json()
 
-                if 'weather' in _weather:
-                    for data in _weather['weather']:
-                        conditions = f'{conditions} {data["main"]}, {data["description"]}'
-                if 'main' in _weather:
-                    temp = _weather['main']['temp']
-                    humidity = _weather['main']['humidity']
+            conditions, temp, humidity = '', '', ''
 
-                result = (conditions, temp, humidity, )
-                return result
+            if 'weather' in _weather:
+                for data in _weather['weather']:
+                    conditions = f'{conditions} {data["main"]}, {data["description"]}'
+            if 'main' in _weather:
+                temp = _weather['main']['temp']
+                humidity = _weather['main']['humidity']
 
-            def __dict__(self):
-                _weather = self.weather
-                _weather_cats = ['conditions', 'temp', 'humidity']
-                weather_dict = []
-                for index in list(range(len(_weather_cats))):
-                    weather_dict.append([_weather_cats[index], _weather[index]])
+            result = (conditions, temp, humidity, )
+            return result
 
-                return dict(weather_dict)
+        def __dict__(self):
+            _weather = self.weather
+            _weather_cats = ['conditions', 'temp', 'humidity']
+            weather_dict = []
+            for index in list(range(len(_weather_cats))):
+                weather_dict.append([_weather_cats[index], _weather[index]])
 
-            def __str__(self):
-                _weather = self.weather
-                weather_str = ''
-                for data in _weather:
-                    weather_str += f'{str(data)} '
+            return dict(weather_dict)
 
-                return weather_str
+        def __str__(self):
+            _weather = self.weather
+            weather_str = ''
+            for data in _weather:
+                weather_str += f'{str(data)} '
+
+            return weather_str
 
 
 @app.route('/')
