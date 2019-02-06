4f9553f38041d2f72a7f6d5d9adf7086bcbcbcae
DEShmelev <arknitos@gmail.com>
2019-02-06 20:57:28 +0300

    Ref #27 Updating token auth function
    Adding role of request initiator user into req params

--- rest/utils.py
+++ rest/utils.py
@@ -9,29 +9,11 @@ def token_auth_required(func, auth_roles='any'):
     @wraps(func)
     def decorated_handler(*args, **kwargs):
         hnd_params = getattr(args[0], 'params', {})
-        auth_accepted = 0
         if hnd_params:
-            mb_uid = int(hnd_params.get('uid', 0))
+            mb_uid = hnd_params.get('uid', 0)
+            if mb_uid:
+                mb_uid = int(mb_uid)
             mb_token = hnd_params.pop('token', 0)
-            #
-            # mb_service = hnd_params.pop('service', 0)
-            # if mb_service:
-            #     mb_user = User.query.filter(User.username == mb_service).first()
-            #     if UserRole.query.filter(UserRole.id == mb_user.role).filter(UserRole.role == 'service').first():
-            #         uid = getattr(mb_user, 'id', 0)
-            #     else:
-            #         uid = 0
-            #
-            # if uid and (User.query.filter(User.id == uid).first()):
-            #     if mb_token:
-            #         usr_token = getattr(User.UserToken.query.filter(
-            #             (User.UserToken.user_id == uid) & (User.UserToken.type == 'user')).first(), 'token', 0)
-            #         if len(mb_token) == 6:
-            #             usr_token = User.UserToken.query.filter(
-            #                 (User.UserToken.user_id == uid) & (User.UserToken.type == 'totp')).first()
-            #             usr_token = pyotp.TOTP(usr_token.token).now()
-            #         if usr_token == mb_token:
-            #             return func(*args, **kwargs)
 
             if mb_token:
                 uid = getattr(
@@ -41,14 +23,19 @@ def token_auth_required(func, auth_roles='any'):
                     'user_id', 0)
                 if uid:
                     uid_user = User.query.filter(User.id == uid).first()
+                    uid_user_role = getattr(UserRole.query.filter(UserRole.id == uid_user.role).first(), 'role', '')
+                    hnd_params['init_role'] = uid_user_role
                     if mb_uid:
-                        if (not auth_accepted) and (uid == mb_uid):
-                            auth_accepted = 1
+                        if uid_user_role in ['admin', 'service']:
+                            return func(*args, **kwargs)
+                        if mb_uid and uid == mb_uid:
+                            return func(*args, **kwargs)
+                        else:
+                            return []
+                    else:
+                        hnd_params['uid'] = str(uid)
 
-                    # if (not auth_accepted) and
-
-            if auth_accepted:
-                return func(*args, **kwargs)
+                    return func(*args, **kwargs)
         return []
 
     return decorated_handler
