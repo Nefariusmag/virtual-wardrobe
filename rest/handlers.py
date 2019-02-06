1a2ceb7f4ebbebd660a49ff3a2bb27496cc792c1
DEShmelev <arknitos@gmail.com>
2019-02-06 20:58:52 +0300

    Ref #27 Updating existing API handlers
    Some bugfixes and refatoring

--- rest/handlers.py
+++ rest/handlers.py
@@ -27,54 +27,59 @@ class APIHandlers(object):
 
     @token_auth_required
     def handler_user(self):
-        user = User.query.filter(User.id == self.params['uid']).one()
+        user = User.query.filter(User.id == self.params['uid']).first()
 
-        if self.params['method'] == 'GET':
-            user = model2dict(user, ['password'])
-            return [200, ('user', user)]
+        if user:
+            if self.params['method'] == 'GET':
+                user = model2dict(user, ['id', 'password'])
+                return [200, ('user', user)]
 
-        if (self.params['method'] == 'POST') and (self.params['data']):
-            # TODO: Add input validation for each field
-            need_update = 0
-            for k, v in self.params['data'].items():
-                cv = getattr(user, k, 0)
-                if cv and (cv != v) and (k not in ['id']):
-                    setattr(user, k, v)
-                    need_update = 1
-                else:
-                    break
+            if (self.params['method'] == 'POST') and (self.params['data']):
+                # TODO: Add input validation for each field
+                need_update = 0
+                for k, v in self.params['data'].items():
+                    cv = getattr(user, k, 0)
+                    if cv and (k not in ['id', 'username']):
+                        setattr(user, k, v)
+                        need_update = 1
+                    else:
+                        break
 
-            if need_update:
-                db.session.commit()
-                return [200, ('msg', "User's data is updated")]
+                if need_update:
+                    db.session.commit()
+                    user = model2dict(user, ['id', 'password'])
+                    return [200, ('msg', "User's data is updated"), ('user', user)]
 
         return []
 
     @token_auth_required
     def handler_clothes(self):
-        user = User.query.filter(User.id == self.params['uid']).one()
+        user = User.query.filter(User.id == self.params['uid']).first()
+        if user:
+            if self.params['method'] == 'GET':
+                if user.clothes:
+                    clothes = [model2dict(clothe) for clothe in user.clothes]
+                else:
+                    clothes = []
+                return [200, ('user', user.username), ('clothes', clothes)]
 
-        if self.params['method'] == 'GET':
-            if user.clothes:
-                clothes = [model2dict(clothe) for clothe in user.clothes]
-            return [200, ('user', user.username), ('clothes', clothes)]
+            if (self.params['method'] == 'POST') and (self.params['data']):
+                need_update = 0
+                n_clothes = self.params['data'].get('clothes', 0)
+                if n_clothes:
+                    for n_clothe in n_clothes:
+                        n_clothe_id = n_clothe.get('id', 0)
+                        if n_clothe_id:
+                            c_clothe = [x for x in user.clothes if x.id == int(n_clothe_id)][0]
+                            for k, v in n_clothe.items():
+                                cv = getattr(c_clothe, k, 0)
+                                if cv and (k not in ['id']):
+                                    setattr(c_clothe, k, v)
+                                    need_update = 1
 
-        if (self.params['method'] == 'POST') and (self.params['data']):
-            need_update = 0
-            n_clothes = self.params['data'].get('clothes', 0)
-            if n_clothes:
-                for n_clothe in n_clothes:
-                    n_clothe_id = n_clothe.get('id', 0)
-                    if n_clothe_id:
-                        c_clothe = [x for x in user.clothes if x.id == int(n_clothe_id)][0]
-                        for k, v in n_clothe.items():
-                            cv = getattr(c_clothe, k, 0)
-                            if cv and (cv != v) and (k not in ['id']):
-                                setattr(c_clothe, k, v)
-                                need_update = 1
-
-            if need_update:
-                db.session.commit()
-                return [200, ('msg', "Clothes's data is updated")]
+                if need_update:
+                    db.session.commit()
+                    clothes = [model2dict(clothe) for clothe in user.clothes if clothe.id in [x['id'] for x in n_clothes]]
+                    return [200, ('msg', "Clothes's data is updated"), ('clothes', clothes)]
 
         return []
